f8eb20ce444d2507e613b9408bb3459484af8b04
hyeonsu0 <s_dail@naver.com>
2021-03-29 18:19:28 +0900

    create

---
+++ myprog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 2021-03-29 soo
+
+mkdir -p temp
+echo "create temp directory"
+mv *.txt temp
+mv mycal.sh temp
+echo "move filed to temp directory"
+
+echo -e "1)add\n2)sub\n3)div\n4)mul\n" 
