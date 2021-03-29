78330f4619ef4e84281c5b9c1d5b299240e7b1d3
hyeonsu0 <s_dail@naver.com>
2021-03-29 19:24:48 +0900

    update

--- mycal.sh
+++ mycal.sh
@@ -1,25 +1,33 @@
 #!bin/bash
 # 2021-03-29 soo
 
-local oper = $1
-num1=$(<num1.txt)
-num2=$(<num2.txt)
+oper=$1
+num1=$(<$2num1.txt)
+num2=$(<$2num2.txt)
 
-if [ -z "$oper" ]; then
-  echo "choice operator 1)add 2)sub 3)div 4)mul"
-  read -p "Enter your choice: " oper
+if [ -z "$1" ]; then
+  echo "none operator parameter"
+  echo -e "choice operator \n1)add\n2)sub\n3)div\n4)mul\n"
+  read -p "your choice: " oper
 fi
 
 case $oper in
   1|add)
-    temp = `expr$num1 + $num2`
-    echo "$num1 + $num2 = $temp"
+    temp=`expr $num1 + $num2`
+    op="add"
+;;
   2|sub)
-    temp = `expr$num1 - $num2`
-    echo "$num1 - $num2 = $temp"
+    temp=`expr $num1 - $num2`
+    op="sub"
+;;
   3|div)
-    temp = `expr$num1 / $num2`
-    echo "$num1 / $num2 = $temp"
+    temp=`expr $num1 / $num2`
+    op="div"
+;;
   4|mul)
-    temp = `expr$num1 * $num2`
-    echo "$num1 * $num2 = $temp"
+    temp=`expr $num1 \* $num2`
+    op="mul"
+;;
+esac
+
+echo -e "num1 : $num1\nnum2 : $num2\nop : $op\ntotal : $temp"
